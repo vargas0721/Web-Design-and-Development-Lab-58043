ddedb7e34cc3be3681be27363725de9d8be4127b
Vargas, Erryl James G <89883123+vargas0721@users.noreply.github.com>
2022-09-30 13:29:49 +0800

    Add files via upload

---
+++ exercise1.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title> Exercise 1 WDD </title>
+</head>
+<body>
+<center>
+
+<?php
+	//declare variables
+	$name = "Erryl James G. Vargas";
+	$college_program = "Computer Engineering";
+	$year = "3rd Year";
+	$section = "Web Design and Development - 58043";
+	$string = "Today, I will learn PHP programming.";
+	
+	//print variables
+	echo "$name";
+	echo "<br>";
+	echo "$college_program";
+	echo "<br>";
+	echo "$year";
+	echo "<br>";
+	echo "$section";
+	echo "<br>";
+	echo $string;
+	echo "<br>";
+	
+?>
+
+
+</body>
+</html>
